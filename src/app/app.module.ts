d2c78dae64e895511b98471419246fa27e85f3bd
Cris Furlin <crisfurlin@gmail.com>
2021-08-21 16:59:13 -0500

    Toolbar improvements

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import {MatIconModule} from '@angular/material/icon';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AppHeaderComponent } from './app-header/app-header.component';
 import { RecipeCardComponent } from './recipe-card/recipe-card.component';
+
 
 @NgModule({
   declarations: [
@@ -14,7 +16,8 @@ import { RecipeCardComponent } from './recipe-card/recipe-card.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
